a65d86bc97dfb2a0df4680e9a9fd3edb513b12d3
xinmaxi7-prog <xinmaxi7@gmail.com>
2026-07-11 01:48:58 +0800

    Delete database.php

--- database.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-BA 174/Pid.Sus|||https://docs.google.com/uc?export=download&id=1rnyqSPv6D3GVmQ_3RXEdWlSdoldjgBG8
-BA 168/Pid.Sus|||https://docs.google.com/uc?export=download&id=1DWofg0RW2mLUCfgo3Tv-2JYH9aHchTef
-Rekaman Sidang Iksar _ Abi|||https://raw.githubusercontent.com/xinmaxi7-prog/file-rekaman/main/260708_1401_iksar%20Abi.mp3
-Rekaman Akbar + Asri _ REVANDO|||https://docs.google.com/uc?export=download&id=1XRRjWz-TmQyO6N4b1uG6TzjQYBaGI7Vy
